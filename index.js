797224f5ae67c94d8fb147f07dcb1e9ac2859631
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-06-16 13:33:41 -0300

    basic requirements

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-const users = [];
-const tweets = [];
+const users = [
+  {
+    username: 'bobesponja', 
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+  }
+];
+
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  }
+];
+
+const getTweets = [
+  {
+    username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+	  tweet: "eu amo o hub"
+  }
+]
 
 app.post('/sign-up', (req, res) => {
   users.push(req.body);
@@ -12,12 +35,16 @@ app.post('/sign-up', (req, res) => {
 
 app.post('/tweets', (req, res) => {
   tweets.push(req.body);
+
+  const user = users.find(user => user.username === req.body.username);
+  getTweets.push({...user, tweet: req.body.tweet});
+
   res.send("OK");
 });
 
 app.get('/tweets', (req, res) => {
-  if (tweets.length > 10) res.send(tweets.slice(-10));
-  else res.send(tweets);
+  if (tweets.length > 10) res.send((getTweets.slice(-10)).reverse());
+  else res.send(getTweets.reverse());
 });
 
 app.listen(5000);
